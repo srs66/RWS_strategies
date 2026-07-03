fba7e29be44e07df38be0642ec73d002a9893db4
srs66 <3771387044@qq.com>
2026-07-04 02:00:23 +0800

    Add files via upload

--- scripts/机枪风筝.js
+++ scripts/机枪风筝.js
@@ -5,9 +5,9 @@ var UC = Packages.com.corrodinggames.rts.strategy.game.units.class_426;
 var UNIT_NAME = "mechMinigun";
 var MAX_RANGE = 210;
 var TICK_GAP = 3;
-var ENGAGE_MARGIN = 3;
+var ENGAGE_MARGIN = 6;
 var SHADOW_TOLERANCE = 0;
-var LOOKAHEAD_BASE = 35;
+var LOOKAHEAD_BASE = 600;
 var LOOKAHEAD_SPEED = 20;
 
 var track = {};
